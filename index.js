e9c428282e9b1918ae7e7cd9a361ed3282222f86
kieyn.parks <kieyn.parks@snhu.edu>
2021-11-21 20:29:33 -0500

    add dynamic background color

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ window.addEventListener('DOMContentLoaded', setup);
 
 
 function setup(){
-  var counter = 0;
-  var cubby = document.getElementsByClassName('b1');
-  var bookLinks = document.querySelectorAll('.book-shelf a');
-  var bookShelf = document.querySelectorAll('.book-shelf');
 
+    var color_code0 = ['yellow', 'red', 'pink', 'blue', 'lightgrey'];
+    var color_code1 = ['tomato', 'brown', 'cyan', 'aqua', 'chocolate'];
+
+
+    var color1 = Math.floor(Math.random() * 5);
+    var color2 = Math.floor(Math.random() * 5);
+
+    var counter = 0;
+    var cubby = document.getElementsByClassName('b1');
+    var bookLinks = document.querySelectorAll('.book-shelf a');
+    var bookShelf = document.querySelectorAll('.book-shelf');
+
+
+    for(var x = 0; x < cubby.length; x++){
+      cubby[x].style.backgroundImage = 'linear-gradient('+ color_code0[color1] +', '+ color_code1[color2] +')';
+    }
 
 
 
